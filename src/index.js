7716785af9c1aecb3adb39e2c0e408452e6956a4
bendance1 <bendance6@gmail.com>
2024-11-02 00:32:21 -0400

    Index update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const CommandProcessor = require('./commands/CommandProcessor');
-
-if (process.stdin.isTTY) {
-  console.log('Please provide commands (press Ctrl+C to exit):');
-  process.stdin.setEncoding('utf8');
-}
-
 const commandProcessor = new CommandProcessor();
 
+console.log('Please provide commands (press Ctrl+C to exit):');
+
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('data', (chunk) => {
+  const input = chunk.toString();
   const commands = chunk.trim().split('\n');
   for (const command of commands) {
     commandProcessor.process(command);
